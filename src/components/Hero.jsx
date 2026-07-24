e49ee2e6ccd88e772a9d94521ff7899c07453aca
Yashraj <umaatuldixit9598990671@gmail.com>
2026-07-24 14:22:34 +0530

    first commit

---
+++ src/components/Hero.jsx
@@ -0,0 +1,35 @@
+function Hero() {
+  return (
+    <section className="hero">
+
+      <div className="left">
+
+
+        <h1>Hi, I'm</h1>
+
+<h2>Yashraj Dixit</h2>
+
+     
+
+        <h3>MCA Student | Frontend Developer</h3>
+
+        <p>
+          I build modern, responsive and animated web applications using
+          React and JavaScript.
+        </p>
+
+        <button>Download Resume</button>
+
+      </div>
+
+      <div className="right">
+
+        <div className="circle"></div>
+
+      </div>
+
+    </section>
+  );
+}
+
+export default Hero;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+function Navbar() {
+  return (
+    <nav>
+      <div className="logo">
+        <h2>Yash.</h2>
+      </div>
+
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Skills</li>
+        <li>Projects</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
